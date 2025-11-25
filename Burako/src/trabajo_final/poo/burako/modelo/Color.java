dfd7107bc489b17a2286f5482e139afbc65a232a
walterzaq <83846224+walterzaq@users.noreply.github.com>
2025-11-24 23:34:22 -0300

    Delete Burako directory

--- Burako/src/trabajo_final/poo/burako/modelo/Color.java
+++
@@ -1,5 +0,0 @@
-package trabajo_final.poo.burako.modelo;
-
-public enum Color {
-    ROJO,NEGRO,AZUL,AMARILLO,COMODIN;
-}
--- Burako/src/trabajo_final/poo/burako/modelo/ConjuntoTomable.java
+++
@@ -1,6 +0,0 @@
-package trabajo_final.poo.burako.modelo;
-import java.util.ArrayList;
-
-public interface ConjuntoTomable {
-    public ArrayList<Ficha> tomar();
-}
