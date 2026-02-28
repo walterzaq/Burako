daf61005f08789c9ffe9fada853b6ab729165acf
walterzaq <83846224+walterzaq@users.noreply.github.com>
2026-02-28 20:56:15 -0300

    Delete Burako directory

--- Burako/src/main/java/trabajo_final/poo/burako/observer/Observable.java
+++
@@ -1,8 +0,0 @@
-package trabajo_final.poo.burako.observer;
-import trabajo_final.poo.burako.modelo.Evento;
-
-public interface Observable {
-    void agregarObservador(Observador observador);
-    void quitarObservador(Observador observador);
-    void notificarObservadores(Evento evento);
-}
--- Burako/src/main/java/trabajo_final/poo/burako/observer/Observador.java
+++
@@ -1,6 +0,0 @@
-package trabajo_final.poo.burako.observer;
-import trabajo_final.poo.burako.modelo.Evento;
-
-public interface Observador {
-    void actualizar(Evento evento);
-}
